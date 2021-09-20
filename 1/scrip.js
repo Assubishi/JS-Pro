bfcebe70411386efa52865c5f226fd1068826411
Assubishi <Khamiyev02@gmail.com>
2021-09-20 22:39:36 +0500

    a

---
+++ 1/scrip.js
@@ -0,0 +1,24 @@
+'use strict';
+
+const products = [
+    {id: 1, title: 'Notebook', price: 2000},
+    {id: 2, title: 'Mouse', price: 20},
+    {id: 3, title: 'Keyboard', price: 200},
+    {id: 4, title: 'Gamepad', price: 50},
+];
+
+const renderProduct = (elements) =>
+    `<div class = "product-item">
+                    <h3>${elements.title}</h3>
+                    <p>${elements.price}</p>
+                    <button class = "buy-btn">Купить</button>
+                </div>`;
+
+
+const renderPage = list => {
+    const productsList = list.map(item => renderProduct(item));
+    console.log(productsList);
+    document.querySelector(".products").innerHTML = productsList;
+};
+
+renderPage(products);
